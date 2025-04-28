9d30dd531f4edb58b72c883343b125b686407e6c
David Suárez Moreno <115156607+davidmoreno13@users.noreply.github.com>
2025-04-28 11:52:02 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,19 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 import os
 
 app = Flask(__name__)
 
-@app.route('/entities')
-def get_entities():
-    url = 'https://context-pre.apsevilla.com/v2/entities'
+# Ruta general que reenviará todas las peticiones
+@app.route('/proxy')
+def proxy_request():
+    target_url = request.args.get('target')
+    if not target_url:
+        return {"error": "Missing 'target' query parameter"}, 400
+
     try:
-        response = requests.get(url, verify=False)
+        # Petición ignorando certificados SSL
+        response = requests.get(target_url, verify=False)
         response.raise_for_status()
-        return jsonify(response.json())
-    except requests.exceptions.RequestException as e:
+        data = response.json()
+
+        return jsonify(data)
+    except Exception as e:
         return {"error": str(e)}, 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3000))  # Leer puerto de variable de entorno
-    app.run(host='0.0.0.0', port=port)        # Escuchar en 0.0.0.0
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)
