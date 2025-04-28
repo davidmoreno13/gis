6a033942bbdb055b192102ba082e8b10912dc03a
David Suárez Moreno <115156607+davidmoreno13@users.noreply.github.com>
2025-04-28 11:13:57 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -7,11 +8,12 @@ app = Flask(__name__)
 def get_entities():
     url = 'https://context-pre.apsevilla.com/v2/entities'
     try:
-        response = requests.get(url, verify=False)  # Ignoramos problemas de SSL
-        response.raise_for_status()  # Lanza error si la respuesta no es 200
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
         return jsonify(response.json())
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}, 500
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    port = int(os.environ.get('PORT', 3000))  # Leer puerto de variable de entorno
+    app.run(host='0.0.0.0', port=port)        # Escuchar en 0.0.0.0
